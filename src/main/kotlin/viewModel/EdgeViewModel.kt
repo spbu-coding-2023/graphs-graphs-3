d1c8115094ae764e15aa35bfdd5742e355764dff
Homka122 <kombaev02@gmail.com>
2024-05-21 01:10:41 +0300

    feat: add viewModels for Undirected Graph, Vertex and Edge

---
+++ src/main/kotlin/viewModel/EdgeViewModel.kt
@@ -0,0 +1,20 @@
+package viewModel
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import model.graph.Edge
+
+class EdgeViewModel<V> (
+    val first: VertexViewModel<V>,
+    val second: VertexViewModel<V>,
+    private val edge: Edge<V>,
+    private val _weightVisibility: State<Boolean>
+) {
+    private var _weight = mutableStateOf(edge.weight)
+    var weight
+        get() = _weight.value
+        set(value) {
+            _weight.value = value
+            edge.weight = value
+        }
+}
