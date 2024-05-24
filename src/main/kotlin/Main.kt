c055d20724081d759ce9f07d3e53b96c69c69058
Homka122 <kombaev02@gmail.com>
2024-05-24 12:49:08 +0300

    feat: add example of clustering to main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,14 @@
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowPlacement
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
+import model.algorithm.Clustering
 import model.graph.UndirectedGraph
 import view.MainView
 import viewModel.UndirectedViewModel
 
-val AMOUNT_NODES = 100
+val AMOUNT_NODES = 500
+val EDGE_CHANGE = 0.05f
 
 val graph = UndirectedGraph<Int>().apply {
     for (i in (0 until AMOUNT_NODES)) {
@@ -15,18 +17,20 @@ val graph = UndirectedGraph<Int>().apply {
 
     for (i in (0 until AMOUNT_NODES)) {
         for (j in (0 until AMOUNT_NODES)) {
-            if (Math.random() < 0.005) {
+            if (Math.random() < EDGE_CHANGE / 100) {
                 addEdge(i, j)
             }
         }
     }
 }
 
-val undirectedViewModel = UndirectedViewModel(graph, false)
+val groups = Clustering(graph).calculate()
+val undirectedViewModel = UndirectedViewModel(graph, false, groups)
 
 fun main() = application {
     Window(
-        onCloseRequest = ::exitApplication
+        onCloseRequest = ::exitApplication,
+        state = rememberWindowState(placement = WindowPlacement.Maximized)
     ) {
         MainView(undirectedViewModel)
     }
