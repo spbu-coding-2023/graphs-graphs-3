1c7f54a59dfb22257dccbe9e9033f9b46b31c8fb
Homka122 <kombaev02@gmail.com>
2024-05-28 23:16:46 +0300

    feat: add header

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,14 @@
-import androidx.compose.ui.window.Window
-import androidx.compose.ui.window.WindowPlacement
-import androidx.compose.ui.window.application
-import androidx.compose.ui.window.rememberWindowState
+import androidx.compose.foundation.window.WindowDraggableArea
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.*
 import model.algorithm.Clustering
 import model.graph.UndirectedGraph
+import view.HeaderView
 import view.MainView
 import viewModel.graph.UndirectedViewModel
 
@@ -28,11 +33,32 @@ val groups = Clustering(graph).calculate()
 val undirectedViewModel = UndirectedViewModel(graph, false)
 
 fun main() = application {
-    Window(
-        onCloseRequest = ::exitApplication,
-        state = rememberWindowState(placement = WindowPlacement.Maximized),
-        undecorated = true,
-    ) {
-        MainView(undirectedViewModel)
+    var isOpen by remember { mutableStateOf(true) }
+    var isMaximized by remember { mutableStateOf(true) }
+    var isMinimize by remember { mutableStateOf(false) }
+    var position: WindowPosition by remember { mutableStateOf(WindowPosition.PlatformDefault) }
+    var headerName by remember { mutableStateOf("Dimabase.db") }
+
+    val windowState = WindowState(
+        placement = if (isMaximized) WindowPlacement.Maximized else WindowPlacement.Floating,
+    )
+
+    if (isOpen) {
+        Window(
+            onCloseRequest = ::exitApplication,
+            state = windowState,
+            undecorated = true,
+        ) {
+            WindowDraggableArea {
+                HeaderView(headerName,
+                    { isOpen = false }, {
+                        isMaximized = !isMaximized
+                    },
+                    isMaximized, { windowState.isMinimized = !windowState.isMinimized })
+            }
+            MainView(
+                undirectedViewModel,
+            )
+        }
     }
 }
