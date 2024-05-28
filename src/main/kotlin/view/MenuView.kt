b0aa24849b07faef41d5681afec8a4d4867e85f9
Homka122 <kombaev02@gmail.com>
2024-05-29 00:22:02 +0300

    feat: add icons for menu

--- src/main/kotlin/view/MenuView.kt
+++ src/main/kotlin/view/MenuView.kt
@@ -1,17 +1,23 @@
 package view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.width
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+
+@Composable
+fun MenuIcon(name: String, description: String, modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(name),
+        contentDescription = description,
+        modifier = modifier.padding(bottom = 10f.dp)
+    )
+}
 
 @Composable
 fun MenuView() {
@@ -19,8 +25,10 @@ fun MenuView() {
     Column(
         Modifier.fillMaxHeight().width(Config.menuWidth.dp).background(color = Color(0xFF3D3D3D)),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "Some menu", color = Color.White, fontSize = 20f.sp)
+        MenuIcon("Nodes.svg", "Add Node", Modifier.padding(top = 25f.dp))
+        MenuIcon("Ribs.svg", "Add Edge")
+        MenuIcon("Clustering.svg", "Clustering")
+        MenuIcon("PageRank.svg", "Analysis graph")
     }
 }
