1eaea30edf043bdbdad9af5bb296bbb1c125078b
Demon32123 <dmitry.sheiko@mail.ru>
2024-05-30 00:24:31 +0300

    feat: created tests for the algorithm for finding bridges in a graph

---
+++ src/test/kotlin/model/algorithm/FindBridgesTest.kt
@@ -0,0 +1,121 @@
+package model.algorithm
+
+import model.graph.DirectedGraph
+import model.graph.UndirectedGraph
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class FindBridgesTest {
+    @Nested
+    inner class `find bridges in directed graph` {
+        @Test
+        fun `class base test`(){
+            val graph = DirectedGraph<Int>()
+
+            graph.addVertex(1)
+            graph.addVertex(2)
+            graph.addVertex(3)
+            graph.addVertex(4)
+            graph.addVertex(5)
+            graph.addVertex(6)
+
+            graph.addEdge(1, 2)
+            graph.addEdge(2, 3)
+            graph.addEdge(3, 1)
+            graph.addEdge(1, 4)
+            graph.addEdge(4, 5)
+            graph.addEdge(5, 6)
+            graph.addEdge(6, 4)
+
+            val bridges = FindBridges(graph).findBridges()
+
+            assertEquals(Pair(bridges[0].first.key, bridges[0].second.key), Pair(1, 4))
+        }
+        @Test
+        fun `crossing multiple bridges`() {
+            val graph = DirectedGraph<Int>()
+
+            graph.addVertex(1)
+            graph.addVertex(2)
+            graph.addVertex(3)
+            graph.addVertex(4)
+            graph.addVertex(5)
+            graph.addVertex(6)
+            graph.addVertex(7)
+
+            graph.addEdge(1, 2)
+            graph.addEdge(2, 3)
+            graph.addEdge(3, 1)
+            graph.addEdge(1, 4)
+            graph.addEdge(4, 5)
+            graph.addEdge(5, 6)
+            graph.addEdge(6, 7)
+            graph.addEdge(7, 5)
+
+            val bridges = FindBridges(graph).findBridges()
+
+            assertEquals(
+                listOf(Pair(bridges[0].first.key, bridges[0].second.key),
+                    Pair(bridges[1].first.key, bridges[1].second.key)),
+                listOf(Pair(4, 5), Pair(1, 4))
+            )
+        }
+    }
+
+    @Nested
+    inner class `find bridges in undirected graph` {
+        @Test
+        fun `class base test`(){
+            val graph = UndirectedGraph<Int>()
+
+            graph.addVertex(1)
+            graph.addVertex(2)
+            graph.addVertex(3)
+            graph.addVertex(4)
+            graph.addVertex(5)
+            graph.addVertex(6)
+
+            graph.addEdge(1, 2)
+            graph.addEdge(2, 3)
+            graph.addEdge(3, 1)
+            graph.addEdge(1, 4)
+            graph.addEdge(4, 5)
+            graph.addEdge(5, 6)
+            graph.addEdge(6, 4)
+
+            val bridges = FindBridges(graph).findBridges()
+
+            assertEquals(Pair(bridges[0].first.key, bridges[0].second.key), Pair(1, 4))
+        }
+        @Test
+        fun `crossing multiple bridges`() {
+            val graph = UndirectedGraph<Int>()
+
+            graph.addVertex(1)
+            graph.addVertex(2)
+            graph.addVertex(3)
+            graph.addVertex(4)
+            graph.addVertex(5)
+            graph.addVertex(6)
+            graph.addVertex(7)
+
+            graph.addEdge(1, 2)
+            graph.addEdge(2, 3)
+            graph.addEdge(3, 1)
+            graph.addEdge(1, 4)
+            graph.addEdge(4, 5)
+            graph.addEdge(5, 6)
+            graph.addEdge(6, 7)
+            graph.addEdge(7, 5)
+
+            val bridges = FindBridges(graph).findBridges()
+
+            assertEquals(
+                listOf(Pair(bridges[0].first.key, bridges[0].second.key),
+                    Pair(bridges[1].first.key, bridges[1].second.key)),
+                listOf(Pair(4, 5), Pair(1, 4))
+            )
+        }
+    }
+}
