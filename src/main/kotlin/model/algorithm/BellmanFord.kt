6e83c86b3eb098f424a98964afbd95050f013f14
Homka122 <kombaev02@gmail.com>
2024-05-31 15:20:03 +0300

    feat: add BellmanFord algorithm

---
+++ src/main/kotlin/model/algorithm/BellmanFord.kt
@@ -0,0 +1,83 @@
+package model.algorithm
+
+import model.graph.Edge
+import model.graph.Vertex
+import model.graph.WeightedGraph
+
+class BellmanFord(private val graph: WeightedGraph) {
+    val parentMap = HashMap<Vertex, Vertex>()
+    val dist = graph.vertices.associateWith { Long.MAX_VALUE }.toMutableMap()
+
+    private fun getDistance(v: Vertex): Long {
+        return dist[v] ?: throw IllegalStateException("Distance don't initialized")
+    }
+
+    fun calculate(start: Vertex, dest: Vertex): List<Edge> {
+        dist[start] = 0
+
+        for (i in 0 until graph.vertices.size - 1) {
+            updateDist { v, u, uDistance, weight ->
+                dist[v] = uDistance + weight
+                parentMap[v] = u
+            }
+
+            if (updateDist()) {
+                // Negative cycle found
+                return listOf()
+            }
+        }
+
+
+        val result = mutableListOf<Edge>()
+        var vertex = dest
+        var parent = parentMap[vertex]
+        while (parent != start) {
+            if (parent == null) return listOf()
+
+            result.add(findEdge(parent, vertex))
+
+            vertex = parent
+            parent = parentMap[vertex]
+        }
+
+        result.add(findEdge(parent, vertex))
+        return result.reversed()
+    }
+
+    private fun updateDist(action: (Vertex, Vertex, Long, Long) -> Unit = { _, _, _, _ -> }): Boolean {
+        var flag = false
+
+        for (v in graph.vertices) {
+            for (u in graph.vertices) {
+                val uDistance = getDistance(u)
+                val vDistance = getDistance(v)
+                if (!isEdgeExists(u, v)) continue
+
+                val weight = getWeight(u, v)
+                if (uDistance != Long.MAX_VALUE && uDistance + weight < vDistance) {
+                    action(v, u, uDistance, weight)
+                }
+            }
+        }
+
+        return flag
+    }
+
+    private fun getWeight(first: Vertex, second: Vertex): Long {
+        return findEdge(first, second).weight
+    }
+
+    private fun findEdge(first: Vertex, second: Vertex): Edge {
+        val adj = graph.adjacencyList[first] ?: throw IllegalStateException("vertex must have adjacency list")
+        val edge = adj.find { it.second == second } ?: throw IllegalStateException("There is no edge between vertices")
+
+        return edge
+    }
+
+    private fun isEdgeExists(first: Vertex, second: Vertex): Boolean {
+        val adj = graph.adjacencyList[first] ?: throw IllegalStateException("vertex must have adjacency list")
+        val edge = adj.find { it.second == second }
+
+        return edge !== null
+    }
+}
---
+++ src/test/kotlin/model/algorithm/BellmanFordTest.kt
@@ -0,0 +1,57 @@
+package model.algorithm
+
+import model.graph.Graph
+import model.graph.Vertex
+import model.graph.WeightedGraph
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class BellmanFordTest {
+    private fun Graph.addVertices(vararg indexes: Int): Pair<Vertex, Vertex> {
+        for (index in indexes.drop(1).dropLast(1)) {
+            addVertex(index)
+        }
+
+        val first = addVertex(indexes.first()) ?: throw IllegalStateException()
+        val second = addVertex(indexes.last()) ?: throw IllegalStateException()
+
+        return first to second
+    }
+
+    private fun Graph.addEdges(vararg edges: Triple<Int, Int, Long>) {
+        for (edge in edges) {
+            addEdge(edge.first, edge.second, edge.third)
+        }
+    }
+
+    private infix fun Pair<Int, Int>.weight(weight: Long): Triple<Int, Int, Long> {
+        return Triple(this.first, this.second, weight)
+    }
+
+    @Nested
+    inner class `Homka` {
+        @Test
+        fun `test 1`() {
+            var first: Vertex
+            var second: Vertex
+
+            val graph = WeightedGraph().apply {
+                val pair = addVertices(1, 2, 3, 4)
+                first = pair.first
+                second = pair.second
+
+                addEdges(
+                    1 to 2 weight 2,
+                    2 to 4 weight -2,
+                    1 to 3 weight -2,
+                    3 to 4 weight 4
+                )
+            }
+
+            val result = BellmanFord(graph).calculate(first, second).map { it.first.key to it.second.key }
+            assertEquals(listOf(1 to 2, 2 to 4), result)
+        }
+    }
+
+}
