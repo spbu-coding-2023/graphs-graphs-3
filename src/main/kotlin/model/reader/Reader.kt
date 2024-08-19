8fd8afef572bc57895de3a60a31795373b1cb8ca
Demon32123 <dmitry.sheiko@mail.ru>
2024-08-19 19:15:42 +0300

    refactor: added the nameGraph parameter to the loadGraph function

--- src/main/kotlin/model/reader/Reader.kt
+++ src/main/kotlin/model/reader/Reader.kt
@@ -4,5 +4,5 @@ import model.graph.Graph
 
 interface Reader {
     fun saveGraph(graph: Graph, filepath: String): Unit
-    fun loadGraph(filepath: String): Graph
+    fun loadGraph(filepath: String, nameGraph: String): Graph
 }
